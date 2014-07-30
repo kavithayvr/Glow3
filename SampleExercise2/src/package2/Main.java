0dd07cf0a9cbfffbe0cb60318cd109715a5d2875
kavitha <kavithayvr@gmail.com>
2014-07-29 23:20:45 -0400

    Added a comment

--- SampleExercise2/src/package2/Main.java
+++ SampleExercise2/src/package2/Main.java
@@ -14,6 +14,7 @@ data.display();
 	private static void displayGreeting(){
 		System.out.println("Hellow happy salespeople");
 		System.out.println("this app shows sales data");
+		System.out.println("test1");
 	}
 
 }
