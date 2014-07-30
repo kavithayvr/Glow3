1f96db4810286f6cc93da5740dc8ed8c79ac52d2
kavitha <kavithayvr@gmail.com>
2014-07-29 23:11:51 -0400

    Initial Checkin of sales tool

---
+++ SampleExercise2/src/package2/Main.java
@@ -0,0 +1,19 @@
+package package2;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SalesData data=new SalesData();
+displayGreeting();
+data.display();
+	}
+	private static void displayGreeting(){
+		System.out.println("Hellow happy salespeople");
+		System.out.println("this app shows sales data");
+	}
+
+}
---
+++ SampleExercise2/src/package2/SalesData.java
@@ -0,0 +1,8 @@
+package package2;
+
+public class SalesData {
+int data[]={0,4,42};
+public void display(){
+	System.out.println("TODO: Implementdatadisplay");
+}
+}
